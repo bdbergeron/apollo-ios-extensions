7c065db04bb4be6475bf844c24814700bb1bbe82
Bradley David Bergeron <brad@bradbergeron.com>
2025-10-31 15:01:00 -0700

    Updates for swift concurrency

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -47,4 +47,13 @@ let package = Package(
       resources: [
         .process("Resources"),
       ]),
+  ],
+  swiftLanguageVersions: [.v5]
+)
+
+for target in package.targets {
+  target.swiftSettings = target.swiftSettings ?? []
+  target.swiftSettings?.append(contentsOf: [
+    .enableExperimentalFeature("StrictConcurrency")
   ])
+}
